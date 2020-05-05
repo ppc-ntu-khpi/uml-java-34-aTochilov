ba3e791508cb39fd8d8de2b98840f49d0826dae6
Andrii Tochylov <andretochilov1@gmail.com>
2020-05-06 02:19:02 +0300

    creating UML diagram

---
+++ src/Bank/Bank.java
@@ -0,0 +1,65 @@
+package Bank;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Bank {
+
+    /**
+     * Default constructor
+     */
+    public Bank() {
+    }
+
+    /**
+     * 
+     */
+    private String name;
+
+    /**
+     * 
+     */
+    private static Customer[]  customers;
+
+    /**
+     * 
+     */
+    private int customersNumber;
+
+
+
+    /**
+     * @param fn 
+     * @param ln
+     */
+    public void addCustomer(String fn, String ln) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getCustomersNumber() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param index 
+     * @return
+     */
+    public static Customer  getCustomer(int index) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param index
+     */
+    public void deleteCustomer(int index) {
+        // TODO implement here
+    }
+
+}
